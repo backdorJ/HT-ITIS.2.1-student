80d68e858134c69c635f531aab437d9023b3fa6d
daaammirka <cdbacjdorz@yandex.com>
2023-09-30 23:31:33 +0300

    Переделан main [<EntryPoint>]

--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,13 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4
+open Hw4.Parser
+open Hw4.Calculator
+
+[<EntryPoint>]
+let main args =
+    try
+        let optionsOfCalc = parseCalcArguments args
+        let result = (calculate optionsOfCalc.arg1 optionsOfCalc.operation optionsOfCalc.arg2)
+        printfn $"Result: {result}" 
+    with e ->
+        printfn $"Smth was wrong: {e.Message}"
+    0
