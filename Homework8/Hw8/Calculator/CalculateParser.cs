7ccae3d7a244a6dcb50495df0eb2f5fe47d70759
dammmirka <cdbacjdorz@yandex.ru>
2023-11-05 19:46:56 +0300

    Добавлен парсер аргументов

---
+++ Homework8/Hw8/Calculator/CalculateParser.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+
+namespace Hw8.Calculator;
+
+public static class CalculateParser
+{
+    public static (double val1, double val2) ParseArgs(string val1, string val2)
+    {
+        var currentVal1 = 0.0;
+        var currentVal2 = 0.0;
+        
+        if (!double.TryParse(val1,NumberStyles.Any, CultureInfo.InvariantCulture,out currentVal1))
+            throw new InvalidDataException(Messages.InvalidNumberMessage);
+        
+        if(!double.TryParse(val2,NumberStyles.Any, CultureInfo.InvariantCulture ,out currentVal2))
+            throw new InvalidDataException(Messages.InvalidNumberMessage);
+
+        return (currentVal1, currentVal2);
+    }
+}
