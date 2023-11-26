4dc2c59762f36e620a80c76f68690eb3787bfeeb
Damir Nabiullin <cdbacjdorz@yandex.ru>
2023-11-26 17:16:55 +0300

    Домашняя работа 11

--- Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
+++ Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
@@ -1,9 +1,19 @@
+using Hw10.ExpressionParsers;
+using Hw11.Expressions.ExpressionConvert;
+using Hw11.Expressions.ExpressionValidate;
+using Hw11.Expressions.Visitor;
+
 namespace Hw11.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<double> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        ExpressionValidates.Validate(expression ?? "");
+        var polakString = ParserExpression.InfixToPostfix(expression!);    
+        dynamic operationExpression = ExpressionTreeConverter.ConvertToTree(polakString);
+        var result = await ApplyExpressionVisitorDispatcher.Visit(operationExpression);
+
+        return result;
     }
 }
