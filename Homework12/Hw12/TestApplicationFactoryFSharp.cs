d373196432c7fa9e73fb0a8a1e51713df7f53ca1
Damir Nabiullin <cdbacjdorz@yandex.ru>
2023-12-06 20:25:31 +0300

    дз 12 выполнено

--- Homework12/Hw12/TestApplicationFactoryFSharp.cs
+++ Homework12/Hw12/TestApplicationFactoryFSharp.cs
@@ -3,6 +3,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 namespace Hw12;
 
 public class TestApplicationFactoryFSharp : WebApplicationFactory<Hw6.App.Startup>
-    // TODO: replace generic argument with the right one
+    // TODO:  replace generic argument with the right one 
 {
 }
